657931f62f57fc10c712c07307f0d72489638e3e
Gary <s310068@shsh.tw>
2021-03-12 13:36:10 +0800

    312

--- CNN.py
+++ CNN.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-
+from imblearn.under_sampling import RandomUnderSampler
 from solve_cudnn_error import *
 
 solve_cudnn_error()
@@ -22,11 +22,20 @@ if __name__ == '__main__':  # For test Class
     data = pd.read_csv('output2.csv', dtype=np.float)
 
     x, y = make_train(data)
-    x = x.reshape(43110, 13, 13, 1)
-    print(y[:20])
-    y = tf.keras.utils.to_categorical(y, 3)
-    print(y[:20])
-    print(x.shape, y.shape)
+    # 43110 13 13
+    orig_shape = x.shape
+    print(orig_shape)
+
+    x = np.reshape(x, (x.shape[0], x.shape[2]*x.shape[1]))
+    print(x.shape)
+
+    X_new, Y_new = RandomUnderSampler().fit_resample(x, y)
+
+
+    X_new = np.reshape(X_new, (X_new.shape[0], orig_shape[1], orig_shape[2], 1))
+    Y_new = tf.keras.utils.to_categorical(Y_new, 3)
+    print(X_new.shape, Y_new.shape)
+
 
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(32, (3, 3), activation='relu',padding='same', input_shape=(13, 13, 1)),
@@ -40,7 +49,7 @@ if __name__ == '__main__':  # For test Class
     ])
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
-    model.fit(x, y, batch_size = 20, epochs = 10, validation_split=0.8)
+    model.fit(X_new, Y_new, batch_size = 20, epochs = 10, validation_split=0.3)
     model.save('model.h5')
 
 
